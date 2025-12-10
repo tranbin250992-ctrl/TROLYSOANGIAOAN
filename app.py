3371cf6015a704f95fc461489f5e4a98d6383780
tranbin250992-ctrl <tranbin250992@gmail.com>
2025-12-10 14:30:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Đinh Trần Anh - Trường Tiểu học Phù Loan - ĐT: 097 9996 212</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Đinh Trần Anh - Trường Tiểu học Phù Loan - ĐT: 097 9996 212</div>", unsafe_allow_html=True)
